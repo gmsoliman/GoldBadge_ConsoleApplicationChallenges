b51e7eceead6145b8763d97a2c08e5704dac7aaf
gmsoliman <gmsoliman13@gmail.com>
2020-10-07 12:07:50 -0400

    finished repo test for challenge 3

--- 03_BadgesTest/BadgesRepoTest.cs
+++ 03_BadgesTest/BadgesRepoTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using _03_BadgesClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,7 +14,7 @@ namespace _03_BadgesTest
         public void Arrange()
         {
             _repo = new BadgesRepo();
-            _badges = new Badges();
+            _badges = new Badges(12345, new List<string> { "A5", "A7" });
         }
         [TestMethod]
         public void AddABadge()
@@ -23,6 +24,41 @@ namespace _03_BadgesTest
 
             //Assert
             Assert.IsTrue(addResult);
+        }
+        [TestMethod]
+        public void GetFullBadgeDictionary()
+        {
+            //Arrange
+            _repo.AddBadgeToDictionary(_badges);
+            
+            //Act
+            Dictionary<int, List<string>> newDictionary = _repo.GetAllBadges();
+
+            //Assert
+            bool dictionaryHasBadges = newDictionary.ContainsKey(_badges.BadgeID);
+            Assert.IsTrue(dictionaryHasBadges);
+        }
+        [TestMethod]
+        public void UpdateBadge()
+        {
+            //Arrange
+            _repo.AddBadgeToDictionary(_badges);
+            string updatedAccess = "A1";
+
+            //Act
+            _repo.UpdateAccess(12345, updatedAccess);
+            Assert.AreEqual(3, _badges.DoorAccess.Count);
+        }
+        [TestMethod]
+        public void RemoveDoorAccess()
+        {
+            //Arrange
+            _repo.AddBadgeToDictionary(_badges);
+            string removedAccess = "A5";
+
+            //Act
+            _repo.RemoveAccess(12345, removedAccess);
+            Assert.AreEqual(1, _badges.DoorAccess.Count);
         }
     }
 }
