8f34e00123e505ad80f967b0375c7463df056cb3
gmsoliman <gmsoliman13@gmail.com>
2020-10-07 10:58:17 -0400

    finished class library for challenge 3

--- 03_BadgesClassLibrary/BadgesRepo.cs
+++ 03_BadgesClassLibrary/BadgesRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,70 @@ namespace _03_BadgesClassLibrary
         {
             Dictionary<int, List<string>> badgeDictionary = GetAllBadges();
             int badgeID = badges.BadgeID;
+            List<string> doorAccess = badges.DoorAccess;
+
+            int badgesCount = _badges.Count();
+            badgeDictionary.Add(badgeID, doorAccess);
+            bool wasAdded = badgesCount > _badges.Count();
+            return wasAdded;
+        }
+
+        //Read (all items)
+        public Dictionary<int, List<string>> GetAllBadges()
+        {
+            return _badges;
+        }
+
+        //Read (one item)
+        public List<string> GetBadgeByID(int badgeID)
+        {
+            List<string> doorAccess;
+            if (_badges.TryGetValue(badgeID, out doorAccess))
+            {
+                return doorAccess;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //Update
+        public void UpdateAccess(int badgeID, string updatedAccess)
+        {
+            foreach (int badgeNumber in _badges.Keys)
+            {
+                if(badgeNumber == badgeID)
+                {
+                    _badges[badgeNumber].Add(updatedAccess);
+                }
+            }
+        }
+
+        //Delete (one door)
+        public void RemoveAccess (int badgeID, string accessToBeRemoved)
+        {
+            foreach (int badgeNumber in _badges.Keys)
+            {
+                if(badgeNumber == badgeID)
+                {
+                    List<string> doorAccess = _badges[badgeNumber];
+                    doorAccess.Remove(accessToBeRemoved);
+                }
+            }
+        }
+
+        //Delete (all doors)
+        public void RemoveAllAccess (int badgeID)
+        {
+            foreach (int badgeNumber in _badges.Keys)
+            {
+                if(badgeID == badgeNumber)
+                {
+                    List<string> doorAccess = _badges[badgeNumber];
+                    doorAccess.Clear();
+                }
+            }
         }
     }
 }
